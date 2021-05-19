daa03fef4c738e2166fcf02950d431300da78072
nicolas lebeault <nicolas.lebeault@diiage.org>
2021-05-19 18:32:30 +0200

    dev algo

---
+++ ToUpper/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ToUpperV1
+{
+    //Version avec deux méthodes différentes
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez écrire une phrase : ");
+            string sentence = Console.ReadLine();            
+            Console.Write(sentence+"=>"+"["+ToUpperEven(sentence)+","+ToUpperOdd(sentence)+"]");
+
+        }
+
+        private static string ToUpperEven(string value)
+        {
+            string res = string.Empty;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    res += value[i].ToString().ToUpper();
+                }
+                else
+                {
+                    res += value[i];
+                }
+            }
+            return res;
+        }
+        private static string ToUpperOdd(string value)
+        {
+            string res = string.Empty;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    res += value[i].ToString().ToUpper();
+                }
+                else
+                {
+                    res += value[i];
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ ToUpper_v2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ToUpperV2
+{
+    //Version avec un paramètre supplémentaire et une méthode
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez écrire une phrase : ");
+            string sentence = Console.ReadLine();
+            Console.Write(sentence + "=>" + "[" + ToUpper(sentence,false) + "," + ToUpper(sentence,true) + "]");
+
+        }
+
+        private static string ToUpper(string value, bool odd)
+        {
+            string res = string.Empty;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (!odd ? i % 2 == 0 : i % 2 != 0)
+                {
+                    res += value[i].ToString().ToUpper();
+                }
+                else
+                {
+                    res += value[i];
+                }
+            }
+            return res;
+        }
+    }
+       
+}
